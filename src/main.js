06995211ceaa1b39188a491f4401d3c7a6fdfea1
Marcelino Meza <kmeza@ucol.mx>
2020-12-11 12:17:14 -0600

    added factorial function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,35 @@
-export default class App {}
+export default class App {
+
+    // Función que reciba un numero y entregue su factorial (for)
+
+    factorial(numero){
+
+        let factorial = 1;
+
+        for(let i=2; i<=numero; i++){
+            if(i<=numero){
+                factorial = factorial * i;
+            }
+        }
+
+        return factorial;
+    }
+
+    // Función que
+
+
+    // Función que
+
+
+    // Función que
+
+    
+}
 
 let app = new App();
+
+// F1
+
+console.log(app.factorial(5));
+console.log(app.factorial(3));
+console.log(app.factorial(1));
